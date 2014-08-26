7b92ebfa322cfe20250023233b046658728cfc1e
Ryan Hiebert <ryan@ryanhiebert.com>
2014-08-26 16:47:20 -0500

    Reduce complexity by removing explicit context
    Global variables are typically a antipattern, but when used judiciously,
    they can be helpful. In this case, I've chosen to use two global
    variables, for workon_dir and for virtual_env. I think this is
    acceptable, because they both get their data from the environment, which
    are global anyway.
    
    I've added empty global variables to the main module level with a
    comment of how they are used to help make their usage understandable.
    Now properly setting those global variables is the job of the pew
    callback, which gets run before every command anyway.
    
    Additionally, I've opted to remove the Context.dirs and Context.envs,
    putting them, with comments, in the ls method, which is the only place
    they were used anyway. This eliminated the need for the context
    entirely, as well as the wierd 'pass_context' decorator.

--- pew.py
+++ pew.py
@@ -14,70 +14,58 @@ __meta__ = type
 windows = sys.platform == 'win32'
 bin = 'Scripts' if windows else 'bin'
 
-
-class Context:
-    """Manage the context of commands"""
-
-    def __init__(self):
-        """Shared script context"""
-        virtual_env = os.environ.get('VIRTUAL_ENV')
-        workon_home = os.path.expanduser(os.environ.get('WORKON_HOME', ''))
-
-        if not workon_home:
-            sys.exit('WORKON_HOME not set')
-
-        self.workon_home = Path(workon_home)
-        self.virtual_env = None
-        if virtual_env:
-            self.virtual_env = Path(virtual_env)
-
-    def envs(self, path):
-        """List all virtual environments in path"""
-        home = self.workon_home / path
-        pythons = home.glob('*/{}/python'.format(bin))
-        envs = set(path.parent.parent.relative_to(home) for path in pythons)
-        return envs
-
-    def dirs(self, path):
-        """List all directories that aren't virtual environments in path"""
-        home = self.workon_home / path
-        dirs = set(f.relative_to(home) for f in home.iterdir() if f.is_dir())
-        return dirs - self.envs(path)
-
-
-pass_context = click.make_pass_decorator(Context, ensure=True)
+# Global variables used by pew
+virtual_env, workon_home = None, None
 
 
 @click.group()
 def pew():
     """Manage your virtual environments"""
+    global virtual_env, workon_home
+
+    virtual_env = os.environ.get('VIRTUAL_ENV')
+    workon_home = os.path.expanduser(os.environ.get('WORKON_HOME', ''))
+
+    if not workon_home:
+        sys.exit('WORKON_HOME not set')
+
+    workon_home = Path(workon_home)
+
+    if virtual_env:
+        virtual_env = Path(virtual_env)
 
 
 @pew.command()
-@pass_context
-def show(ctx):
+def show():
     """Show the active virtual environment"""
-    click.echo(ctx.virtual_env or 'No virtual environment active.')
+    click.echo(virtual_env or 'No virtual environment active.')
 
 
 @pew.command()
 @click.argument('path', default='')
-@pass_context
-def ls(ctx, path):
+def ls(path):
     """List available virtual environments"""
-    envs = [str(env) for env in ctx.envs(path)]
-    dirs = [str(dir) + '/' for dir in ctx.dirs(path)]
-    click.echo(' '.join(sorted(envs + dirs)))
+    home = workon_home / path
+
+    # Virtual environments
+    pythons = home.glob('*/{}/python*'.format(bin))
+    envs = {path.parent.parent.relative_to(home) for path in pythons}
+
+    # Directories that aren't virtual environments
+    dirs = {f.relative_to(home) for f in home.iterdir() if f.is_dir()} - envs
+
+    listables = [str(env) for env in envs]
+    directories = [str(dir) + '/' for dir in dirs]  # Add slash to directories
+    click.echo(' '.join(sorted(environments + directories)))
 
 
 @pew.command(name='in')
 @click.argument('env')
 @click.argument('command', required=False)
 @click.argument('args', nargs=-1)
-@pass_context
-def inve(ctx, env, command, args):
+def inve(env, command, args):
     """Enter a virtual environment"""
-    path = ctx.workon_home / env
+    path = workon_home / env
     if not path.exists():
         sys.exit("Environment '{}' does not exist.".format(env))
 
@@ -106,22 +94,20 @@ def inve(ctx, env, command, args):
 @pew.command()
 @click.argument('env')
 @click.option('--python', '-p', help='Path to Python interpreter.')
-@pass_context
-def new(ctx, env, python):
+def new(env, python):
     """Create a new virtual environment"""
-    path = ctx.workon_home / env
+    path = workon_home / env
     extra = ['--python={}'.format(python)] if python else []
     check_call(['virtualenv', str(path)] + extra)
 
 
 @pew.command()
 @click.argument('envs', nargs=-1)
-@pass_context
-def rm(ctx, envs):
+def rm(envs):
     """Remove virtual environments."""
     for env in envs:
-        path = ctx.workon_home / env
-        if path == ctx.virtual_env:
+        path = workon_home / env
+        if path == virtual_env:
             sys.exit('You cannot remove the active environment.')
         shutil.rmtree(str(path))
 
