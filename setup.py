de2b628ab9f8361b431293cb9c1db135a5072227
Ryan Hiebert <ryan@ryanhiebert.com>
2014-09-03 11:06:01 -0500

    Upgrade pathlib for 2.6 support

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         'setuptools>=0.7',
         'click>=3',
-        'pathlib>=1',
+        'pathlib>=1.0.1',
     ],
     entry_points={'console_scripts': ['pew = pew:pew']},
     classifiers=[
